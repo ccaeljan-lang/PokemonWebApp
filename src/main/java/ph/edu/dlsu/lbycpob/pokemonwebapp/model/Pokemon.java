a231500c75ebc5a5b6393ee10b0858fe69b010e4
de-rekk <painganshawnderek@gmail.com>
2026-07-22 01:28:57 +0800

    Added constructor to Pokemon class

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
@@ -2,5 +2,8 @@ package ph.edu.dlsu.lbycpob.pokemonwebapp.model;
 
 // Concrete Pokemon implementation
 public class Pokemon extends AbstractPokemon {
-
+    public Pokemon(int instanceId, String name, double weight, double height,
+                   double attack, double defense, double stamina, String type) {
+        super(instanceId, name, weight, height, attack, defense, stamina, type);
+    }
 }
