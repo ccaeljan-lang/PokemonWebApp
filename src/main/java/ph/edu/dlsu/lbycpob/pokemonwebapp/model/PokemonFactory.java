016d576b8031e4a957d58f5a65ba0e4c35bd02f1
caeljandcristobal <ccaeljan@gmail.com>
2026-07-22 13:51:48 +0800

    Imported AtomicInteger utility in PokemonFactory

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/PokemonFactory.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/PokemonFactory.java
@@ -1,5 +1,7 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp.model;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 // Pokemon Factory
 public class PokemonFactory {
     private static final AtomicInteger counter = new AtomicInteger(0);
